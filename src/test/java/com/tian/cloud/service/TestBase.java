72e14f161cdc061baf1d8de8a60d7df11e8000e2
tianguang <tianguang@fenbi.com>
2018-08-29 20:15:59 +0800

    keep

---
+++ src/test/java/com/tian/cloud/service/TestBase.java
@@ -0,0 +1,8 @@
+package com.tian.cloud.service;
+
+/**
+ * @author tianguang
+ * 2018/8/29 下午8:15
+ **/
+public class TestBase {
+}
---
+++ src/test/java/com/tian/cloud/service/service/impl/SituationServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.tian.cloud.service.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SituationServiceImplTest {
+
+    @Test
+    public void saveOrUpdate() {
+    }
+}
