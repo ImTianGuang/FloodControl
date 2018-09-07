f2a7a449038ebfc3855b455df4155b9ebc5e1f92
tianguang <tianguang@fenbi.com>
2018-09-07 18:54:13 +0800

    keep

--- src/main/java/com/tian/cloud/service/dao/entity/FloodUser.java
+++ src/main/java/com/tian/cloud/service/dao/entity/FloodUser.java
@@ -1,10 +1,21 @@
 package com.tian.cloud.service.dao.entity;
 
+import lombok.Data;
+
 /**
  * @author tianguang
  * 2018/9/7 下午6:52
  **/
+@Data
 public class FloodUser {
 
     private int id;
+
+    private String userId;
+
+    private String wechatUserId;
+
+    private String userName;
+
+    private String password;
 }
