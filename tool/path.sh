48addacd130465369cc70d944af44f8b07945a0a
tianguang <tianguang@fenbi.com>
2018-12-31 17:55:46 +0800

    path

--- tool/path.sh
+++ tool/path.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 sudo mkdir /var/log/flood/
-
 sudo chmod 777 /var/log/flood/
+
+sudo mkdir /var/file/
+sudo mkdir /var/file/export/
+sudo mkdir /var/file/upload/
+sudo chmod 777 /var/file/
+sudo chmod 777 /var/file/*
