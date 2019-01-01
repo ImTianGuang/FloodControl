c9cf89773fdddb3af79e20da4df546202b7e73bd
tianguang <tianguang@fenbi.com>
2019-01-01 15:19:34 +0800

    test

--- tool/deploy.sh
+++ tool/deploy.sh
@@ -6,5 +6,8 @@ else
   runEnv=local
 fi
 mvn clean package -Dmaven.test.skip=true
+echo "kill the process..."
+ps aux | grep common-web.jar | awk 'NR==1{print $2}'|xargs kill -9
 echo "nohup java -jar -Dspring.profiles.active=$runEnv target/common-web.jar > /dev/null 2>&1 &"
 nohup java -jar -Dspring.profiles.active=$runEnv target/common-web.jar > /dev/null 2>&1 &
+ps aux | grep common-web.jar
