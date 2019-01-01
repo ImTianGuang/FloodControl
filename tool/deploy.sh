482e07ad23c8dcba89021207581d6e26370bb033
tianguang <tianguang@fenbi.com>
2019-01-01 18:54:53 +0800

    test

--- tool/deploy.sh
+++ tool/deploy.sh
@@ -7,7 +7,7 @@ else
 fi
 mvn clean package -Dmaven.test.skip=true
 echo "kill the process..."
-ps aux | grep common-web.jar | awk 'NR==1{print $2}'|xargs kill -9
+ps aux | grep common-web.jar | awk '{print $2}' |xargs kill -9
 echo "nohup java -jar -Dspring.profiles.active=$runEnv target/common-web.jar > /dev/null 2>&1 &"
 nohup java -jar -Dspring.profiles.active=$runEnv target/common-web.jar > /dev/null 2>&1 &
 ps aux | grep common-web.jar
